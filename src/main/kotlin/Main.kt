cc0fca16e6adc7420f84e3462dd5ea24dfcc91bc
levon <levonkhachatrian7@gmail.com>
2024-03-11 13:33:16 +0400

    main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,29 @@
 package org.example
 
+import kotlin.reflect.KProperty
+
 fun main() {
-    println("Hello World!")
+    val student = Student()
+    student.age = 25
+    println(student.age)
+    println(student.age)
+
+}
+
+class Student {
+    var age: Int by MyLazy()
+}
+
+class MyLazy<T> {
+    private var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        println("getting")
+        return value ?: throw NullPointerException()
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        println("setting")
+        this.value = value
+    }
 }
